e0a5cfb67589b9820e03ce78d59ec538aefec966
Laura <laura.e.vollmer@gmail.com>
2025-09-08 13:07:42 -0500

    Added DndProvider to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css'
 import RoomButton from "./components/RoomSelector"
 import FurniturePalette from './components/FurniturePalette';
-import {furnitureByRoom} from "./data/FurnitureItem"
+import { furnitureByRoom } from "./data/FurnitureItem"
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 function App() {
 
   return (
     <>
-      <div>
-        <h1>hello</h1>
-        <RoomButton/>
-      </div>
+      <DndProvider backend={HTML5Backend}>
+        <div>
+          <h1>hello</h1>
+          <RoomButton />
+        </div>
+      </DndProvider>
     </>
   )
 }
