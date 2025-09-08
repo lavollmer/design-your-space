06e314a58e0052f8fe7253fe166e8f468ced019a
Laura <laura.e.vollmer@gmail.com>
2025-09-08 13:13:40 -0500

    Revising code in FurniturePalette component based on documentation of drag and drop

--- src/components/FurniturePalette.jsx
+++ src/components/FurniturePalette.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 // useDrag hook 
-import {useDrag} from 'react-dnd';
-import FurnitureItem from "../data/FurnitureItem"
+import { useDrag } from 'react-dnd';
+import ItemTypes from "../data/FurnitureItem"
 
 const FurniturePalette = () => {
-    const [{isDragging}, drag] = useDrag (() => ({
+    const [{ isDragging }, drag] = useDrag(() => ({
         type: ItemTypes.FurnitureItem,
-        collect:(monitor) => ({
+        collect: (monitor) => ({
             isDragging: !!monitor.isDragging()
         })
     }))
 
-  return (
-    <div>FurniturePalette</div>
-  )
+    return (
+        <div ref={drag} style={{
+            opacity: isDragging ? 0.5 : 1,
+            fontSize: 25,
+            fontWeight: 'bold',
+            cursor: 'move',
+        }}>
+            <h1>hello</h1>
+        </div>
+    )
 }
 
 export default FurniturePalette
