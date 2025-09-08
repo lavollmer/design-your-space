b01de4346ed9fea442b0f61c4b02dabf2825977c
Laura <laura.e.vollmer@gmail.com>
2025-09-08 11:55:35 -0500

    Reorganizing website structure and starting on FurniturePalette

---
+++ src/components/FurniturePalette.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FurniturePalette = () => {
+  return (
+    <div>FurniturePalette</div>
+  )
+}
+
+export default FurniturePalette
--- src/components/RoomSelector.jsx
+++ src/components/RoomSelector.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// roomselector lets the user to decide which room they are designing
+// when user selects a new background or layout on design with different furniture items
 const RoomSelector = ({ selectedRoom, onSelectRoom }) => {
     // rooms array
     const room = ["Living Room", "Bedroom", "Office"]
