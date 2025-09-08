243150672b3fafe4f2ad2f8ab9bc501e9c249c51
Laura <laura.e.vollmer@gmail.com>
2025-09-08 12:04:41 -0500

    Updating the data file furniture with new asset image names

--- src/data/FurnitureItem.js
+++ src/data/FurnitureItem.js
@@ -1,10 +1,8 @@
 export const furnitureByRoom = {
     "Living Room": [
-        {id: 'sofa', name: 'Sofa', img: ''},
+        {id: 'sofa', name: 'sofa', img:'sofa.png'},
+        {id: 'lamp', name: 'lamp', img:'lamp.png'},
+        {id: 'table', name: 'table', img:'table.png'},
     ]
-
-
-
-
 
 }
