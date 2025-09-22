81fac19bbab445edcb7dd8561a2d3cae02e36379
Laura <laura.e.vollmer@gmail.com>
2025-09-22 11:04:04 -0500

    Coding new assets into furniture item jsx

--- src/data/FurnitureItem.jsx
+++ src/data/FurnitureItem.jsx
@@ -4,15 +4,22 @@ export const furnitureByRoom = {
         { id: 'sofa', name: 'Sofa', img: '/assets/sofa.png' },
         { id: 'lamp', name: 'Lamp', img: '/assets/lamp.png' },
         { id: 'table', name: 'Table', img: '/assets/table.png' },
+        { id: 'chair', name: 'Chair', img: '/assets/chair_livingroom.png' },
+        { id: 'hanging chair', name: 'Hanging Chair', img: '/assets/hanging_chair.png' },
+        { id: 'rug', name: 'Rug', img: '/assets/rug.png' }
     ],
     "Bedroom": [
         { id: 'bed', name: 'Bed', img: '/assets/bed.png' },
         { id: 'plant', name: 'Plant', img: '/assets/plant.png' },
         { id: 'nightstand', name: 'Nightstand', img: '/assets/nightstand.png' },
+        { id: 'dresser', name: 'Dresser', img: '/assets/dresser.png' },
+        { id: 'rug', name: 'Rug', img: '/assets/rug.png' }
     ],
     "Office": [
         { id: 'desk', name: 'Desk & Chair', img: '/assets/desk.png' },
         { id: 'mirror', name: 'Mirror Art', img: '/assets/mirror.png' },
         { id: 'office_plant', name: 'Plant', img: '/assets/office_plant.png' },
+        {id:'rug', name: 'Rug', img: '/assets/rug.png'},
+        {id: 'hanging plant', name: 'Hanging Plant', img: '/assets/hanging_plant.png'}
     ],
 }
